ca25e30b7386aadd6ee917c40baeb355087adb2d
Jeffrey <1115378579@qq.com>
2022-09-06 17:06:48 +0800

    feat: 添加回到顶部 添加随机功能

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,11 +6,16 @@ const images = _.chunk(imgs, imgs.length / 3)
 <template>
   <div flex="~ wrap" max-w="1480px" justify="center" ma class="wrapper">
     <div v-for="groupingImg, index in images" :key="index" max-w="33%">
-      <div v-for="image in groupingImg" :key="image" m-1 rd overflow-hidden>
+      <div
+        v-for="image in groupingImg.sort(() => {
+          return 0.5 - Math.random()
+        })" :key="image" m-1 rd overflow-hidden
+      >
         <a-image width="100%" :src="`images/${image}`" />
       </div>
     </div>
   </div>
+  <a-back-top />
 </template>
 
 <route lang="yaml">
